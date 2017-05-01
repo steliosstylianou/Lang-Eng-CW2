91093c8ead0f8a3a6e4191515d8be1b3212cb0c7
Stelios Stylianou <stylianoustelioscy@gmail.com>
2017-05-01 22:52:39 +0100

    dynammic in progress

--- Parser.hs
+++ Parser.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MultiWayIf #-}
 
 import Prelude hiding (Num)
 import Debug.Trace
 
 import System.IO
 import Control.Monad
-import Text.ParserCombinators.Parsec hiding (State)
+import Text.ParserCombinators.Parsec hiding (State, parse)
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
+import Data.List
+import Data.Maybe
 
 type Num = Integer
 type Var = String
@@ -170,12 +173,13 @@ parse' str =
       Left e  -> error $ show e
       Right r -> r
 
-parseFile :: String -> IO Stm
+{-- parseFile :: String -> IO Stm
 parseFile file =
  do program  <- readFile file
     case parse whileParser "" program of
       Left e  -> print e >> fail "parse error"
       Right r -> return r
+      --}
 
 --LEXER
 
@@ -220,6 +224,8 @@ semisep    = Token.semiSep    lexer
 symbol     = Token.symbol     lexer
 
 
+------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 --EVALUATION FUNCTIONS
 
@@ -228,7 +234,11 @@ n_val :: Num -> Z
 n_val a = a
 
 s :: State
-s _    = 0
+--s_fun "x" = 5
+s _ = 0;
+
+
+
 --s "x" = 3
 --s "y" = 0
 --s "z" = 0
@@ -256,6 +266,11 @@ update st i v v2
 
 scope_stm = Block [("x",N 0)] [("p",Ass "x" (Mult (V "x") (N 2))),("q",Call "p")] (Block [("x",N 5)] [("p",Ass "x" (Add (V "x") (N 1)))] (Comp (Call "q") (Ass "y" (V "x"))))
 
+
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
 type Loc = Integer
 data Config = Inter Stm State | Final State
 type Store = Loc -> Z  --store location's value
@@ -264,13 +279,14 @@ type EnvP = Pname -> Stm
 
 next = 0
 
---s_static :: Stm -> State -> State
---s_mixed  :: Stm -> State -> State
---s_dynamic :: Stm -> State -> State
-
+------------------------------------------------------------------------
+-- STATIC
+------------------------------------------------------------------------
+{--
 new :: Loc->Loc
 new loc = loc + 1
 
+upd_P ::
 update_store :: Store -> Z -> Var -> State
 update_store st i v v2
       | v == v2 = i
@@ -283,69 +299,85 @@ lookup envv sto = sto . envv
 s_static :: Stm -> State -> State
 s_static stm s = undefined
 
+s_eval :: EnvV -> EnvP -> Config -> Config --pg 57
+s_eval envv envp (Inter (Ass x a) s) = Final (update s (a_val a s) x)
+s_eval envv envp (Inter (Skip) s) = Final s
+s_eval envv envp (Inter (Comp s1 s2) s) = s_eval envv envp (Inter s2 s) . s_eval envv envp (Inter s1 s)
+s_eval envv envp (Inter (If bexp s1 s2) s)
+                          | b_val bexp == True  = s_eval envv envp (Inter s1 s)
+                          | b_val bexp == False = s_eval envv envp (Inter s2 s)
+s_eval envv envp (Inter (While b ss) s)
+                          | b_val b == True  = Final s2
+                          | b_val b == False = s_eval envv envp (Skip) s
+                           where
+                           Final s1 = s_eval envv envp ((Inter ss s) s)
+                           Final s2 = s_eval (Inter (While b ss) s1)
+s_eval envv envp (Inter (Block decv decp stm) s) = undefined --d_eval envv (d_updateEnvPs envp envp stm) Inter stm
+s_eval envv envp (Inter (Call name) s) = undefined --d_eval (Inter (envp name) s)
 
+--}
+
+------------------------------------------------------------------------
+-- DYNAMIC
+------------------------------------------------------------------------
+
+startEnvp :: EnvP
+startEnvp = Skip
 
 s_dynamic :: Stm -> State -> State
-s_dynamic stm s = s'
- where
-  Final s' = d_eval (Inter stm s) 
+s_dynamic stm s = undefined
 
-d_eval :: EnvV -> EnvP -> Config -> Config --pg 57
-d_eval envv envp (Inter (Ass x a) s) = Final (update s (a_val a s) x)
-d_eval envv envp (Inter (Skip) s) = Final s
-d_eval envv envp (Inter (Comp s1 s2) s) = d_eval envv envp (Inter s2 s) . d_eval envv envp (Inter s1 s)
-d_eval envv envp (Inter (If bexp s1 s2) s) 
-                          | b_val bexp == True  = d_eval envp (Inter s1) s
-                          | b_val bexp == False = d_eval envp (Inter s2) s
-                            where 
-d_eval envv envp (Inter (While b ss) s)
-                          | b_val b == True  = Final s2
-                          | b_val b == False = d_eval envp (Skip) s
-                           where 
-                           Final s1 = d_eval (Inter ss s)
-                           Final s2 = d_eval (Inter (While b ss) s1)
-d_eval envv envp (Inter (Block decv decp stm) s) = d_eval envv (d_updateEnvPs envp envp stm) Inter stm 
-d_eval envv envp (Inter (Call name) s) = d_eval (Inter (envp name) s)
+d_eval :: EnvP -> Config -> Config --pg 57
+d_eval  envp (Inter (Ass x a) s) = Final (update s (a_val a s) x)
+d_eval  envp (Inter (Skip) s) = Final s
+d_eval  envp (Inter (Comp s1 s2) s) = Final state
+                             where
+                             Final state1 = d_eval envp (Inter s1 s)
+                             Final state = d_eval envp (Inter s2 state1)
+d_eval  envp (Inter (If bexp s1 s2) s)
+                          | b_val bexp s = d_eval envp (Inter s1 s)
+                          | otherwise = d_eval envp (Inter s2 s)
+d_eval envp (Inter (While b ss) s)
+                          | b_val b s  = Final s2
+                          | otherwise  = Final s
+                           where
+                           Final s1 = d_eval envp (Inter ss s)
+                           Final s2 = d_eval envp (Inter (While b ss) s1)
+d_eval  envp (Inter (Block decv decp stm) s) = Final s_retain s (s_dynamic_eval (decP_eval dp e) s1 (decV_eval dv s)) dv
 
+d_eval  envp (Inter (Call name) s) = d_eval envp (Inter (envp name) s)
 
-d_updateEnvPs :: EnvP -> DecP -> EnvP
-d_updateEnvPs env ((pname,stms):others)  = d_updateEnvP others (envP_check env stms pname)	--pg55
-d_updateEnvPs env [] = env
+d_updateDps :: EnvP -> DecP -> EnvP
+d_updateDps env ((pname,stms):others) = d_updateDps (d_updateDp env (pname, stms)) others  --pg55
+d_updateDps env [] = env
 
-d_updateEnvP :: Envp -> Pname -> Stm -> Envp
-d_updateEnvP (pname,stm) p s 
-                 | pname == p = s
-                 | otherwise  = (pname,stm)
+d_updateDp :: EnvP -> (Pname, Stm) -> EnvP
+d_updateDp envp (pname,stm) = \p -> if
+                            | p == pname -> stm
+                            | otherwise  -> envp p
 
-d_updateEnvPs :: DecV -> State -> State
-d_updateEnvPs decv state   = d_updateEnvP others (envP_check env stms pname)	--pg55
-d_updateEnvPs env [] = env
+------------------------------------------------------------------------
+-- MIXED
+------------------------------------------------------------------------
+{--
+s_mixed :: Stm -> State -> State
+s_mixed stm s   = undefined
 
-d_updateEnvP :: Envp -> Pname -> Stm -> Envp
-d_updateEnvP (pname,stm) p s 
-                 | pname == p = s
-                 | otherwise  = (pname,stm)
+runDynamic :: [(Var, Z)] -> Stm -> [(Var, Z)]
+runDynamic vars stm = extractState (s_dynamic stm (createState vars)) (fst (unzip vars))
 
+runMixed :: [(Var, Z)] -> Stm -> [(Var, Z)]
+runMixed vars stm = extractState (s_mixed stm (createState vars)) (fst (unzip vars))
 
+runStatic :: [(Var, Z)] -> Stm -> [(Var, Z)]
+runStatic vars stm = extractState (s_static stm (createState vars)) (fst (unzip vars))
 
+createState :: [(Var, Z)] -> State
+createState vars x = case elemIndex x (fst (unzip vars)) of
+    Just index -> snd (vars !! index)
+    Nothing -> 0
 
+extractState :: State -> [Var] -> [(Var, Z)]
+extractState state var_names = map (\var_name -> (var_name, state var_name)) var_names
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--}
